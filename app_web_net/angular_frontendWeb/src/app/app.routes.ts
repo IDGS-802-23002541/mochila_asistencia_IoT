a3e4d80372a93c640af57bc2f95c6dc0e39fac70
IDGS-802-22000969 <79263@alumnos.utleon.edu.mx>
2026-07-09 19:55:12 -0600

    Agrega layout y paginas de inicio y login

--- app_web_net/angular_frontendWeb/src/app/app.routes.ts
+++ app_web_net/angular_frontendWeb/src/app/app.routes.ts
@@ -1,9 +1,44 @@
 import { Routes } from '@angular/router';
+
 import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Inicio } from './pages/inicio/inicio';
+import { MainLayout } from './layout/main-layout/main-layout';
+
 
 export const routes: Routes = [
+
   {
     path: '',
     component: Home
+  },
+  {
+    path: 'login',
+    component: Login
+  },
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: 'inicio',
+        component: Inicio
+      },
+
+      // {
+      //   path: 'dispositivos',
+      //   component: Dispositivos
+      // },
+
+      // {
+      //   path: 'mapa',
+      //   component: Mapa
+      // },
+
+      // {
+      //   path: 'graficas',
+      //   component: Graficas
+      // }
+    ]
   }
 ];
