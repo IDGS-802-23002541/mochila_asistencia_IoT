b4c6edf35e8734de95fccf0d6a886c66fe137544
IDGS-902-22000969 <79263@alumnos.utleon.edu.mx>
2026-08-05 19:33:48 -0600

    cambios cotizacion

--- app_web_net/angular_frontendWeb/src/app/pages/organizaciones/organizaciones-list/organizaciones-list.ts
+++ app_web_net/angular_frontendWeb/src/app/pages/organizaciones/organizaciones-list/organizaciones-list.ts
@@ -2,10 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { OrganizacionesService } from '../../../services/organizaciones';
 import { Organizacion } from '../../../interfaces/organizacion';
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/danna
 @Component({
   selector: 'app-organizaciones-list',
   standalone: true,
